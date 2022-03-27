3c4d837dc97f16aa32e843253a8bc299a2e0bd90
Hamid Razavi <razavi.hamid85@gmail.com>
2022-03-27 17:21:08 +0900

    adding new method to my calculator.

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -11,13 +11,11 @@ def test_calculator_is_instance():
     # checking that calculator variable contains an instance of Calculator class
     assert isinstance(calculator, Calculator)
 
-
 def test_calculator_get_result_method():
     """Testing the Calculator"""
     calculator = Calculator()
     # this checks the calculator get result method
     assert calculator.get_result() == 0
-
 
 def test_calculator_result_property():
     """Testing the Calculator"""
@@ -29,25 +27,21 @@ def test_calculator_result_property():
     assert calc1.result == 8
     assert calc2.result == 9
 
-
 def test_calculator_add_method():
     """Testing the Calculator"""
     calculator = Calculator()
     # this is show using the calculator object add method
     assert calculator.add(1, 1) == 2
 
-
 def test_calculator_subtract_method():
     """Testing the Calculator Subtract"""
     calculator = Calculator()
     assert calculator.subtract(1, 1) == 0
 
-
 def test_calculator_multiply_method():
     """Testing the Calculator Subtract"""
     calculator = Calculator()
     assert calculator.multiply(1, 1) == 1
-
 
 def test_my_first_test_add():
     """Testing the simplest addition"""
@@ -55,6 +49,28 @@ def test_my_first_test_add():
 
 def test_my_first_test_add_with_variables():
     """Testing the simplest addition"""
+    value_a = 1
+    value_b = 2
+    value_c = value_a + value_b
+    assert value_a + value_b == value_c
+
+def test_my_first_test_subtract():
+    """Testing the simplest subtraction"""
+    assert 1 - 1 == 0
+
+def test_my_first_test_subtract_with_variables():
+    """Testing the simplest subtraction"""
+    value_a = 1
+    value_b = 2
+    value_c = value_a + value_b
+    assert value_a + value_b == value_c
+
+def test_my_first_test_multiply():
+    """Testing the simplest multiplication"""
+    assert 1 * 1 == 1
+
+def test_my_first_test_multiply_with_variables():
+    """Testing the simplest multiplication"""
     value_a = 1
     value_b = 2
     value_c = value_a + value_b
